46aae3835bd8181eca3f2d4fe8b2cde36858d3d9
Son DANG <son@tinypulse.com>
2017-06-25 21:32:30 +0700

    Seed Messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if User.all.count < 1
+  30.times do
+    User.create(
+      email: Faker::Internet.email,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      password: '123456',
+      display_name: Faker::Name.name,
+      avatar: Faker::Avatar.image
+    )
+  end
+end
 
-30.times do
-  User.create(
-    email: Faker::Internet.email,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    password: '123456',
-    display_name: Faker::Name.name,
-    avatar: Faker::Avatar.image
-  )
+if Message.all.count < 1
+  sender = User.find_by_id(1)
+  receiver = User.find_by_id(2)
+  20.times do
+    Message.create(
+      subject: Faker::Job.title,
+      sender_id: sender.id,
+      recipent_id: receiver.id,
+      body: Faker::Friends.quote
+    )
+  end
 end
