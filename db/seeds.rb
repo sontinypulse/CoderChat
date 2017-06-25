4a4ed609f981b4b092f2c547976f06f09107b31f
Son DANG <son@tinypulse.com>
2017-06-25 20:42:19 +0700

    Seed User

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
+30.times do
   User.create(
     email: Faker::Internet.email,
     first_name: Faker::Name.first_name,
