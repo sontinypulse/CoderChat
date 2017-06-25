28d6cb8b64171db94a853c4a9af4dbfbe3d04988
Son DANG <son@tinypulse.com>
2017-06-25 10:57:18 +0700

    JS not work

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module CoderChat
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.precompile += %w(home.scss users.scss users.js messages.scss messages.js)
+    config.assets.raise_production_errors = true
   end
 end
